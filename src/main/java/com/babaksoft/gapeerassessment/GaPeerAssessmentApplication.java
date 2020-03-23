3b9f087b074cc200e95c38a8e2b8ae1d3f636c07
Babak <babak@sce.carleton.ca>
2020-03-23 04:54:13 -0400

    project is created with initial mvn dependencies

---
+++ src/main/java/com/babaksoft/gapeerassessment/GaPeerAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.babaksoft.gapeerassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaPeerAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaPeerAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/babaksoft/gapeerassessment/GaPeerAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babaksoft.gapeerassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaPeerAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
